907d0f527e24946c505f6fa8391a644433770702
koizr <koizumi.ryo.dev@gmail.com>
2020-04-26 23:06:13 +0900

    ESLint + Prettier

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+const path = require("path");
+
+module.exports = {
+    webpack: config => {
+        config.resolve.alias = {
+            ...config.resolve.alias,
+            "@": path.resolve(__dirname, 'src'),
+        };
+        return config;
+    },
+};
